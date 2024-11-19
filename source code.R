bd046f5ce0d684580264db92e528860d9339cd97
Amanda M. Wilson <47330626+awilson12@users.noreply.github.com>
2024-11-19 15:50:11 -0700

    Update source code.R
    Adding code for figure 2

--- source code.R
+++ source code.R
@@ -106,4 +106,57 @@ View(frame.summary)
   log10(4.9E-3/5.8E-4)
   log10(330/39)
   log10(330/4.9E-3)
+
+ 
+  #------------- generating figure 2------------------------------
+    
+    #run QMRA model (single fomite touch and daily fomite exposure)
+    QMRA_model(threshold=0.19,model=modelrun[4])
+    
+    #save output from single fomite touch model
+    save.single.touch.list<-QMRA_model_output_single_touch
+    
+    #save output from daily fomite exposure model
+    save.day.touch.list<-QMRA_model_output_day_touch
+    
+    
+    #------------ single fomite touch plot--------------------------------------------------------------
+    
+    #extracting data from list to do plotting
+    data<-save.single.touch.list
+    data$threshold_compliance<-"No"
+    data$threshold_compliance[data$risk_single<data$threshold]<-"Yes"
+    data$maximum_allowable_conc<-min(data$C.surface[data$threshold_compliance=="No"])
+    
+    p=ggplot(data)+geom_point(aes(x=C.surface,y=risk_single,color=threshold_compliance))+
+      geom_hline(yintercept=data$threshold[1],color="grey",linetype="dashed",lwd=1)+
+      geom_vline(xintercept=data$maximum_allowable_conc[1],color="black",linetype="solid",lwd=1)+
+      scale_x_continuous(trans="log10",name=expression("Concentration on Surfaces (viral particles/cm"^2*")"))+
+      scale_y_continuous(trans="log10",name="Infection Risk from Single Fomite Touch")+
+      #geom_label(aes(label=paste("Maximum Allowable Conc.",round(max_allow_single,2)),x=max_allow_single,y=1e-8),fill="white")+
+      #geom_label(aes(label=paste("Risk Threshold",round(data$threshold[1],3)),x=1e-3,y=data$threshold[1]),fill="white")+
+      scale_color_manual(name="Threshold Compliance",values=c("#E69F00", "#56B4E9"))+
+      theme(legend.position="top",axis.text=element_text(size=12),axis.title=element_text(size=12),legend.title=element_text(size=12),
+            legend.text=element_text(size=12))
+      
+    #-------------- daily fomite touch plot---------------------------------------------------------------------------
+    
+    data2<-save.day.touch.list
+    data2$threshold_compliance<-"No"
+    data2$threshold_compliance[data2$risk_daily<data2$threshold]<-"Yes"
+    data2$maximum_allowable_conc<-min(data2$C.surface[data2$threshold_compliance=="No"])
+    
+    p2<-ggplot(data2)+geom_point(aes(x=C.surface,y=risk_daily,color=threshold_compliance))+
+      geom_hline(yintercept=data2$threshold[1],color="grey",linetype="dashed",lwd=1)+
+      geom_vline(xintercept=data2$maximum_allowable_conc[1],color="black",linetype="solid",lwd=1)+
+      scale_x_continuous(trans="log10",name=expression("Concentration on Surfaces (viral particles/cm"^2*")"))+
+      scale_y_continuous(trans="log10",name="Infection Risk from Daily Fomite Touch")+
+      #geom_label(aes(label=paste("Maximum Allowable Conc.",round(max_allow_daily,2)),x=max_allow_daily,y=1e-8),fill="white")+
+      #geom_label(aes(label=paste("Risk Threshold",round(data2$threshold[1],3)),x=1e-2,y=data2$threshold[1]),fill="white")+
+      scale_color_manual(name="Threshold Compliance",values=c("#E69F00", "#56B4E9"))+
+      theme(legend.position="top",axis.text=element_text(size=12),axis.title=element_text(size=12),legend.title=element_text(size=12),
+            legend.text=element_text(size=12))
+   
+  
+  
   
